ecc78d62608ef097d2b26de009330dbfc4901026
vvishnevsky <victor.vishnevsky@gmail.com>
2023-03-15 08:56:09 +0200

    hw4 project added

---
+++ hw4/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace hw4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+            //Write a C# method to find the largest value from three integer values.
+
+            
+            //Write a C# method to find the lowest value from three integer values.
+
+
+        }
+    }
+}
