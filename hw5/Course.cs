12ee7d151a60dc9c54409d813af0dcba50136c23
vvishnevsky <victor.vishnevsky@gmail.com>
2023-03-21 07:05:53 +0200

    add classes

--- hw5/Course.cs
+++ hw5/Course.cs
@@ -2,5 +2,21 @@
 {
     class Course
     {
+        private string _courseName { get; set; }
+        private string _teacherName { get; set; }
+        private int _courseDuration { get; set; }
+        private int _students { get; set; }
+
+        public Course()
+        {
+        }
+
+        public void Print()
+        {
+        }
+
+        public void Add()
+        {
+        }
     }
 }
