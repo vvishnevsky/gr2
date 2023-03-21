b7d85ea096e1774dec2862976619384d806d2a79
Victor vishnevsky <vvishnevsky@deloitte.com>
2023-03-21 19:14:26 +0200

    student class

--- hw5/Program.cs
+++ hw5/Program.cs
@@ -5,6 +5,24 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            Console.Write("Enter first name: ");
+            var firstName = Console.ReadLine();
+            Console.Write("Enter second name: ");
+            var lastName = Console.ReadLine();
+            Console.Write("Enter age: ");
+            var age = int.Parse(Console.ReadLine());
+            Console.Write("Enter city: ");
+            var city = Console.ReadLine();
+
+            var student1 = new Student(firstName, lastName, age, city);
+            
+            student1.AddCourse("course1");
+            student1.AddCourse("course2");
+            student1.AddCourse("course10");
+            student1.Print();
+
+            student1.DeleteCourse("course2");
+            student1.Print();
         }
     }
 }
--- hw5/Student.cs
+++ hw5/Student.cs
@@ -2,24 +2,55 @@
 {
     class Student
     {
-        private string _firstName {  get; set; }
-        private string _lastName { get; set; }
-        private int _age { get; set; }
-        private string _city { get; set; }
-        private string[] _coursesAttended { get; set; }
+        private string FirstName { get; set; }
+        private string LastName { get; set; }
+        private int Age { get; set; }
+        private string City { get; set; }
+        private string[] CoursesAttended { get; set; }
 
-        public Student()
+        public Student(string firstName, string lastName, int age, string city)
         {
+            CoursesAttended = new string[10];
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            City = city;
         }
 
         public void Print()
         {
-
+            Console.WriteLine($"\nStudent {FirstName} {LastName}, age {Age}, city {City}");
+            var i = 0;
+            foreach (var c in CoursesAttended)
+            {
+                if (c != null)
+                    Console.WriteLine($"courses attended: {++i}. {c}");
+            }
         }
 
-        public void AddCourse()
+        public void AddCourse(string course)
         {
+            for (var i = 0; i < CoursesAttended.Length; i++)
+                if (CoursesAttended[i] == null)
+                {
+                    CoursesAttended[i] = course;
+                    break;
+                }
+        }
 
+        public void DeleteCourse(string course)
+        {
+            var temp = new string[CoursesAttended.Length];
+            var i = 0;
+            foreach (var c in CoursesAttended)
+            {
+                if (course != c && c != null)
+                {
+                    temp[i] = c;
+                    i++;
+                }
+            }
+            CoursesAttended = temp;
         }
     }
 }
