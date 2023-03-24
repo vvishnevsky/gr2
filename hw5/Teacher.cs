d419aeda8ee42a72164d09e01d7098f7e7a3052d
vvishnevsky <victor.vishnevsky@gmail.com>
2023-03-24 22:36:36 +0200

    hw6 done

--- hw5/Teacher.cs
+++ hw5/Teacher.cs
@@ -1,6 +1,18 @@
 ﻿namespace hw5
 {
-    class Teacher
+    class Teacher : Person
     {
+        public Teacher(string firstName, string lastName, int age, string city) : base(firstName, lastName, age, city)
+        {
+            FirstName = firstName;
+            LastName = lastName;
+            Age = age;
+            City = city;
+        }
+
+        public override void DescribeYourself()
+        {
+            Console.WriteLine($"Hello, i'm a teacher. I was born in {City} and i'm {Age} years old.");
+        }
     }
 }
