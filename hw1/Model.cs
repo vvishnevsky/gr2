5d5ca6dc93b10013b4bb7a51af996ec4ac564c88
Victor vishnevsky <vvishnevsky@deloitte.com>
2023-04-11 23:57:31 +0300

    hw1

---
+++ hw1/Model.cs
@@ -0,0 +1,14 @@
+namespace hw1
+{
+    public class Model
+    {
+        public string? Manufacturer { get; set; }
+        public string? Name { get; set; }
+        public string? Layout { get; set; }
+        public string? Colour { get; set; }
+        public string? Body { get; set; }
+        public string? Class { get; set; }
+        public string? Engine { get; set; }
+        public string? Transmission { get; set; }
+    }
+}
