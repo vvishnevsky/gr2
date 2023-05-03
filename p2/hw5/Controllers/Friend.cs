5f3902c06379a29adf2a068b62ad95c08c36a6c0
Victor vishnevsky <vvishnevsky@deloitte.com>
2023-05-03 18:57:34 +0300

    hw5

---
+++ p2/hw5/Controllers/Friend.cs
@@ -0,0 +1,83 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace hw5.Controllers
+{
+    public class FriendController : Controller
+    {
+        // GET: Friend
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: Friend/Details/5
+        public IActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Friend/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Friend/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Friend/Edit/5
+        public IActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Friend/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Friend/Delete/5
+        public IActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Friend/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ p2/hw5/Models/Friend.cs
@@ -0,0 +1,10 @@
+﻿namespace hw5.Models
+{
+    public class Friend
+    {
+        public int FriendID { get; set; }
+        public string FriendName { get; set; }
+        public string Place { get; set; }
+
+    }
+}
